e36c6c25001ac4ad8f5db5968dc0382a6c2d6438
401Sh <barash1a@gmail.com>
2025-03-11 18:33:22 +0000

    feat: add swagger to users module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Get, Param, UseGuards, Request, Delete, HttpStatus, Patch, Res, ParseUUIDPipe } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 import { AccessTokenGuard } from 'src/guards/accessToken.guard';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Response } from 'express';
 
 @ApiTags('users')
+@ApiBearerAuth()
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
@@ -13,6 +14,8 @@ export class UsersController {
 
   @UseGuards(AccessTokenGuard)
   @Get()
+  @ApiOperation({ summary: 'Получить всех пользователей' })
+  @ApiResponse({ status: 200, description: 'Список пользователей успешно получен' })
   async findAll() {
     return await this.usersService.findAll();
   };
@@ -20,6 +23,8 @@ export class UsersController {
 
   @UseGuards(AccessTokenGuard)
   @Get('self')
+  @ApiOperation({ summary: 'Получить данные о себе' })
+  @ApiResponse({ status: 200, description: 'Информация о пользователе успешно получена' })
   async findMe(@Request() req) {
     const userId = req.user['sub'];
     return await this.usersService.findById(userId);
@@ -28,6 +33,16 @@ export class UsersController {
 
   @UseGuards(AccessTokenGuard)
   @Get(':userId')
+  @ApiOperation({ summary: 'Найти пользователя по ID' })
+  @ApiParam({
+    name: 'userId',
+    type: 'string',
+    format: 'uuid',
+    required: true,
+    description: 'UUID пользователя'
+  })
+  @ApiResponse({ status: 200, description: 'Информация о пользователе успешно получена' })
+  @ApiResponse({ status: 404, description: 'Пользователь не найден' })
   async findById(@Param('userId', ParseUUIDPipe) userId: string) {
     return await this.usersService.findById(userId);
   };
@@ -35,6 +50,12 @@ export class UsersController {
 
   @UseGuards(AccessTokenGuard)
   @Patch('self')
+  @ApiOperation({ summary: 'Обновить данные о себе' })
+  @ApiBody({
+    description: 'Данные для обновления пользователя',
+    type: UpdateUserDto,
+  })
+  @ApiResponse({ status: 200, description: 'Пользователь успешно обновлен' })
   async updateMe(
     @Body() updateUserDto: UpdateUserDto,
     @Request() req
@@ -52,6 +73,8 @@ export class UsersController {
 
   @UseGuards(AccessTokenGuard)
   @Delete('self')
+  @ApiOperation({ summary: 'Удалить свой аккаунт' })
+  @ApiResponse({ status: 204, description: 'Пользователь успешно удален' })
   async deleteMe(
     @Request() req,
     @Res() res: Response
